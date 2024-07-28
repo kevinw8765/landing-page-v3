b3190a54532744b6d7c676f26fd81931b9ead10c
Kevin Wong <kevinw8765@gmail.com>
2024-07-28 19:59:46 -0400

    fix

---
+++ src/components/Experience.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import c from "../assets/c.png"
+import css from "../assets/css.png"
+import github from "../assets/github.png"
+import html from "../assets/html.png"
+import java from "../assets/java.png"
+import javascript from "../assets/javascript.png"
+import node from "../assets/node.png"
+import python3 from "../assets/python.png"
+import pytorch from "../assets/pytorch.png"
+import reactImage from "../assets/react.png"
+import tailwind from "../assets/tailwind.png"
+
+
+
+const Experience = () => {
+
+  const techs = [
+    {
+        id: 1,
+        src: c,
+        title: "C",
+        style: "shadow-blue-500"
+    },
+    {
+        id: 2,
+        src: css,
+        title: "CSS",
+        style: "shadow-blue-500"
+    },
+    {
+        id: 3,
+        src: github,
+        title: "GitHub",
+        style: "shadow-white"
+    },
+    {
+        id: 4,
+        src: html,
+        title: "HTML",
+        style: "shadow-orange-500"
+    },
+    {
+        id: 5,
+        src: java,
+        title: "Java",
+        style: "shadow-orange-500"
+    },
+    {
+        id: 6,
+        src: javascript,
+        title: "JavaScript",
+        style: "shadow-yellow-500"
+    },
+    {
+        id: 7,
+        src: node,
+        title: "Node JS",
+        style: "shadow-green-500"
+    },
+    {
+        id: 8,
+        src: python3,
+        title: "Python",
+        style: "shadow-blue-500"
+    },
+    {
+        id: 9,
+        src: pytorch,
+        title: "PyTorch",
+        style: "shadow-orange-500"
+    },
+    {
+        id: 10,
+        src: reactImage,
+        title: "React",
+        style: "shadow-teal-500"
+    },
+    {
+        id: 11,
+        src: tailwind,
+        title: "Tailwind",
+        style: "shadow-blue-300"
+    },
+    
+  ]
+  return (
+
+    <div name = "experience" className = "w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+        <div className = "max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+            
+            <div>
+                <p className = "text-4xl font-bold border-b-4 inline border-gray-500">
+                    Experience
+                </p>
+            </div>
+
+            <div className = "w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+                {techs.map(({id, src, title, style}) => (
+                    <div key = {id} className = {`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
+                        <img src = {src} alt = "" className = "w-20 mx-auto"/>
+
+                        <p className = "mt-4">{title}</p>
+                    </div>
+                ))}
+            </div>
+
+             
+        </div>
+    </div>
+  )
+}
+
+export default Experience
---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import HeroImage from "../assets/heroImage.png"
+import { FaArrowRight } from "react-icons/fa";
+import {Link} from 'react-scroll'
+
+const Home = () => {
+  return (
+    <div name = "home" className = "pt-16 h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 text-white">
+        <div className = "max-w-screen-lg mx-auto  flex flex-col items-center h-full justify-center px-4 md:flex-row">
+            <div>
+                <h2 className = "text-4xl sm:text-7xl font-bold">I'm an Aspiring Software Engineer</h2>
+
+                <p className = "text-gray-500 py-6 max-w-md">I am a student at RU looking to gain experience in building and designing software. 
+                    I am interested in web development technologies
+                     like React, Tailwind CSS, and JavaScript.</p>
+                <div>
+                    <Link to ="portfolio" smooth duration = {500} smoothclassName = "group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+                        Portfolio
+                        <span className = "group-hover:rotate-90 duration-300">
+                            <FaArrowRight size={25} className = "ml-1"/>
+                        </span>
+                    </Link>
+                </div>
+            </div>
+
+            <div>
+                <img src = {HeroImage} alt = "toy poodle" className = "rounded-2xl mx-auto w-2/3 md:w-4/5"></img>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
